aed1e0da1a9c26ec59884ee904350624a98c8b21
alireza <theethanwardhere@gmail.com>
2025-10-26 13:45:37 +0330

    first commit

---
+++ src/main/java/com/arcade/bootapplication2/BootApplication2Application.java
@@ -0,0 +1,13 @@
+package com.arcade.bootapplication2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/arcade/bootapplication2/BootApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.bootapplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
